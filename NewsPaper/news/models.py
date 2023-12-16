ecf9bb8136cd42c94d02720be3e1c0eedab23194
Oleg2Pi <polikarpovoleg115@gmail.com>
2023-12-16 23:45:51 +0300

    Create models, objects, methods, fill database

--- NewsPaper/news/models.py
+++ NewsPaper/news/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from django.contrib.auth.models import User
+from news.recourse import POSITIONS, news
+from django.db.models import Sum
 
-# Create your models here.
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(default=0)
+
+    def update_rating(self):
+        rating_news = self.post_set.all().aggregate(Sum('rating'))['rating__sum']
+        rating_user = self.user.comment_set.all().aggregate(Sum('rating'))['rating__sum']
+        rating_comment = Comment.objects.filter(post__author=self).aggregate(Sum('rating'))['rating__sum']
+        self.rating = rating_news * 3 + rating_user + rating_comment
+        self.save()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+
+class Post(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    position = models.CharField(max_length=2,
+                                choices=POSITIONS,
+                                default=news)
+    time_create = models.DateTimeField(auto_now_add=True)
+    category = models.ManyToManyField(Category, through='PostCategory')
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    rating = models.IntegerField(default=0)
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
+
+    def preview(self):
+        return self.text[:124] + '...'
+
+
+class PostCategory(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    time_create = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
---
+++ NewsPaper/news/recourse.py
@@ -0,0 +1,7 @@
+news = 'NE'
+article = 'AR'
+
+POSITIONS = [
+    (news, 'Новость'), 
+    (article, 'Статья'),
+]
