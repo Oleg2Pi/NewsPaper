54567c510e3bb6d208b1ff8aa11152f35d881e9e
Oleg2Pi <polikarpovoleg115@gmail.com>
2023-12-19 11:11:39 +0300

    Modificed filter and add in post and posts

--- NewsPaper/news/templatetags/custom_filter.py
+++ NewsPaper/news/templatetags/custom_filter.py
@@ -3,10 +3,25 @@ from django import template
 
 register = template.Library()
 
+STRING_NOT = [
+    'дискриминация',
+]
+
+ZNAKI = ',.;!?-'
 
 @register.filter()
-def censor(string):
-    if isinstance(string, str):
-        return f'{string[0]}' + '*' * (len(string)-1)
+def censor(text):
+    if isinstance(text, str):
+        text1 = []
+        for string in text.split():
+            for string_not in STRING_NOT:
+                if string.lower().startswith(string_not[:-4]):
+                    if string[-1] in ZNAKI:
+                        string = string[0] + '*' * len(string[1:-1]) + string[-1]
+                    else:
+                        string = string[0] + '*' * len(string[1:-1])
+            text1.append(string)
     else:
         raise TypeError
+    
+    return " ".join(text1)
