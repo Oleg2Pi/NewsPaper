9931e795fd3979a0911bda5d6bfade2bb2bf517e
Oleg2Pi <polikarpovoleg115@gmail.com>
2024-01-04 14:15:30 +0300

    Add in views PostsSearch and modificate urls

--- NewsPaper/news/urls.py
+++ NewsPaper/news/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import PostsList, PostDetail
+from .views import (
+    PostsList, PostDetail,
+)
 
 
 urlpatterns = [
--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,5 +1,8 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.views.generic import ListView, DetailView
 from .models import Post
+from .filters import PostsFilter
 
 
 class PostsList(ListView):
@@ -9,6 +12,22 @@ class PostsList(ListView):
     context_object_name = 'posts'
     paginate_by = 10
 
+class PostsSearch(ListView):
+    model = Post
+    ordering = '-time_create'
+    template_name = 'posts_search.html'
+    context_object_name = 'posts'
+    paginate_by = 10
+
+    def get_queryset(self) -> QuerySet[Any]:
+        queryset = super().get_queryset()
+        self.filterset = PostsFilter(self.request.GET, queryset)
+        return self.filterset.qs
+    
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['filterset'] = self.filterset
+        return context
 
 class PostDetail(DetailView):
     model = Post
